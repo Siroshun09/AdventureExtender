82f4f4bd4ad66b0360095f5c7cbffc03313afdca
Siroshun09 <Siroshun09@users.noreply.github.com>
2021-05-17 04:20:27 +0000

    deploy: e8d72325b93ed0c17bbd5adebbf0d91e0a4615ed

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.siroshun09.adventureextender"},{"l":"com.github.siroshun09.adventureextender.loader.message"},{"l":"com.github.siroshun09.adventureextender.loader.translation"},{"l":"com.github.siroshun09.adventureextender.util"}]
